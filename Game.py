c1170ee64feaaaf0acad709dadbe58edaef835ca
kevin.gerhardt <kevin.gerhardt@stud.tu-darmstadt.de>
2021-09-26 12:16:05 +0200

    Added Buttons to the GUI, fived bugs in Game.randomized() and adjusted the backtracking solver

--- Game.py
+++ Game.py
@@ -1,6 +1,8 @@
+import random
 import random as r
 import math
 import sudoku_solver
+
 
 class Game:
 
@@ -9,7 +11,7 @@ class Game:
         self.height = n
         self.board = [[0 for i in range(0, self.width)] for j in range(0, self.height)]
         self.preset = [[False for i in range(0, self.width)] for j in range(0, self.height)]
-        print(self.randomize(0.3))
+        print(self.randomize())
 
     def visualize(self):
         for i in range(0, self.width):
@@ -21,18 +23,29 @@ class Game:
                     print(' | ', end="")
                 print(self.board[i][j], end="")
 
-    def randomize(self, probability):
+    def randomize(self):
+        possible_amount_of_clues = [18, 19, 20]
+        amount_of_clues = possible_amount_of_clues[random.randrange(0, 3, 1)]
         count = 0
+        probability = amount_of_clues/(self.width*self.height)
+
         for i in range(0, self.height):
             for j in range(0, self.width):
-                n = r.random()
-                if n < probability:
-                    num = r.randrange(1, 10, 1)
-                    while not self.can_be_placed(i, j, num):
+                self.preset[i][j] = False
+                self.board[i][j] = 0
+        while count < amount_of_clues:
+            for i in range(0, self.height):
+                for j in range(0, self.width):
+                    n = r.random()
+                    if n < probability:
                         num = r.randrange(1, 10, 1)
-                    self.preset[i][j] = True
-                    self.board[i][j] = num
-                    count += 1
+                        while not self.can_be_placed(i, j, num):
+                            num = r.randrange(1, 10, 1)
+                        self.preset[i][j] = True
+                        self.board[i][j] = num
+                        count += 1
+                        if count == amount_of_clues:
+                            return count
         return count
 
     def is_solved(self):
@@ -42,6 +55,15 @@ class Game:
                 if not self.can_be_placed(i, j, self.board[i][j]):
                     return False
         return True
+
+    def get_free_cell(self):
+        for i in range(0, self.height):
+            for j in range(0, self.width):
+                if self.preset[i][j]: continue
+                if not self.preset[i][j] and self.board[i][j] == 0:
+                    return (i, j)
+
+        return None
 
     def enter_number(self, i, j, num):
         if self.preset[i][j]:
@@ -57,8 +79,8 @@ class Game:
         for k in range(0, self.height):
             if self.board[k][j] == num and k != i:
                 return False
-        square_x = math.floor(j/3)
-        square_y = math.floor(i/3)
+        square_x = math.floor(j / 3)
+        square_y = math.floor(i / 3)
         for a in range(0, 3):
             for b in range(0, 3):
                 row = square_y * 3 + a
@@ -68,6 +90,3 @@ class Game:
                         return False
 
         return True
-
-
-
--- Gui.py
+++ Gui.py
@@ -1,36 +1,62 @@
 import tkinter as tk
 import Util as util
+import Game
+import sudoku_solver
+
 
 class Gui:
 
     def __init__(self, game):
         self.game = game
-        self.t = tk.Tk()
         self.selected_cell = None
+        self.t = tk.Tk()
         self.t.title('Sudoku')
-        self.cnv = tk.Canvas(self.t, width=450, height=450, bg='#000000')
-        self.cnv.grid(columnspan=9, rowspan=9)
         self.cells = [[None for i in range(0, self.game.width)]
                       for j in range(0, self.game.height)]
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.cnv = tk.Canvas(self.t, width=450, height=450, bg='#3b2275')
+        self.cnv.grid(columnspan=9, rowspan=9)
         self.init_grid()
+        solve_button = tk.Button(self.cnv, text="Solve Sudoku!", command=self.solve_button_pressed)
+        solve_button.grid(column=10, row=0, columnspan=5)
+
+        generate_button = tk.Button(self.cnv, text="Generate new Game!", comman=self.generate_button_pressed)
+        generate_button.grid(column=10, row=3)
+
+    def solve_button_pressed(self):
+        solver = sudoku_solver.sudoku_solver(self.game)
+        if self.game.is_solved(): return
+        if solver.solve():
+            self.update()
+            self.game.visualize()
+        else:
+            print("There is no solution to this Sudoku!")
+
+    def generate_button_pressed(self):
+        self.game.randomize()
+        self.update()
+        self.repaint_background()
+        self.update()
 
     def init_grid(self):
 
         for i in range(0, 3):
             for j in range(0, 3):
-                c = tk.Canvas(self.cnv, width=150, height=150,highlightbackground='#000000',
-                                                                     highlightcolor ='#000000', bg='#000000', bd=0)
+                c = tk.Canvas(self.cnv, width=150, height=150, highlightbackground='#000000',
+                              highlightcolor='#000000', bg='#000000', bd=0)
                 c.grid(row=i * 3, column=j * 3, columnspan=3, rowspan=3, ipadx=0, pady=0)
                 for k in range(0, 3):
                     for l in range(0, 3):
-                        d = self.cells[i * 3 + k][j*3+l] = tk.Canvas(self.cnv, width=50,
-                                                                     highlightbackground='#000000',
-                                                                     highlightcolor='#000000',height=50, bg='#FFFFFF')
+                        d = self.cells[i * 3 + k][j * 3 + l] = tk.Canvas(self.cnv, width=50,
+                                                                         highlightbackground='#000000',
+                                                                         highlightcolor='#000000', height=50,
+                                                                         bg='#FFFFFF')
                         d.grid(row=i * 3 + k, column=l + 3 * j, rowspan=1, columnspan=1)
         self.update()
         self.repaint_background()
         self.add_bindings()
-
 
     def add_bindings(self):
         for i in range(0, self.game.height):
@@ -39,6 +65,7 @@ class Gui:
 
                 def handler(event, cells=self.cells, i=i, j=j, gui=self):
                     return util.cell_clicked(event, cells, i, j, gui)
+
                 d.bind('<ButtonPress-1>', handler)
 
                 def handler_b(event, gui=self):
@@ -53,7 +80,7 @@ class Gui:
                 d = self.cells[i][j]
                 d.delete("all")
                 d.create_text(25, 25, fill="#000000", font="Times 20 italic bold",
-                                   text="" if self.game.board[i][j] == 0 else str(self.game.board[i][j]))
+                              text="" if self.game.board[i][j] == 0 else str(self.game.board[i][j]))
 
     def repaint_background(self):
         for i in range(0, self.game.height):
